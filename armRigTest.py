13bd78e6baf917e7a8387dec0d3c27381350fdf9
Eva S <37833950+Mudzii@users.noreply.github.com>
2020-04-18 19:45:05 +0200

    Update armRigTest.py

--- armRigTest.py
+++ armRigTest.py
@@ -19,52 +19,100 @@ def Distance(objA, objB):
 	
     return sqrt(pow(vecA[0]-vecB[0],2)+pow(vecA[1]-vecB[1],2)+pow(vecA[2]-vecB[2],2))
 
+# ================================ # 
+def CreateStarCTRL(CTRL_name, rad):
+    nurbCTRL = cmds.circle( n = str(CTRL_name), nr =(1,0,0), c=(0, 0, 0), r= rad )
+    
+    curveCVs = cmds.ls('{0}.cv[:]'.format(CTRL_name), fl = True)
+    selList = [curveCVs[0], curveCVs[2], curveCVs[4], curveCVs[6]]
+    
+    sel = pm.select(selList)
+    pm.scale(sel, [0.5, 0.5, 0.5])
+    
+    pm.delete(nurbCTRL[0] , ch = 1)
+    pm.makeIdentity(nurbCTRL[0], apply = True )
+    
+    offset_GRP = pm.group( em=True, name= str(CTRL_name) + '_offset_GRP' )
+    pm.parent(nurbCTRL[0], offset_GRP)
+    
+
 # ================================ #
 def CreateTwistJnt(jntRadius, jntName, side, prntJnt, nxtJnt, moveConst, Reparent):
     
     twistJnt = cmds.duplicate(str(prntJnt), n= str(side) + str(jntName), parentOnly=True)[0]
     pm.joint(twistJnt, e=True, rad = jntRadius)
     
+    # move jnt between 2 jnts 
     if moveConst is True:
         pm.parent(twistJnt, world=True )
         constr = pm.parentConstraint( prntJnt, nxtJnt, twistJnt, sr = ["x", "y", "z"], mo = False, w=1 )    
         pm.delete(constr)
-      
+    
+    # reparent jnts   
     if Reparent is True:  
         pm.parent(twistJnt, prntJnt)
     
+    # add new jnt to list
     ind = jntList.index(prntJnt)
     jntList.insert(ind + 1, twistJnt)
 
 
 # ================================ # 
+def CreateIK(jntIKList):
+    
+    side = str(jntIKList[0][0:2])
+    
+    CTRL_name = str(side) + "arm_IK_CTRL"
+    CreateStarCTRL(CTRL_name, 0.6)
+    
+    tempConst = pm.parentConstraint(jntIKList[2], str(CTRL_name) + '_offset_GRP')
+    pm.delete(tempConst)
 
-def DuplicateChain(jnList):
     
-    IKshoulder = cmds.duplicate(str(jnList[1]), n= str(jnList[1] + '_IK'), parentOnly=True)[0]
-    pm.parent(IKshoulder, world=True )
+
+# ================================ # 
+def IK_FKChain(jnList):
     
-    IKelbow = cmds.duplicate(str(jnList[2]), n= str(jnList[2]) + '_IK', parentOnly=True)[0]
-    pm.parent(IKelbow, IKshoulder)
+    global IKJntList
+    global FKJntList
     
-    IKwrist = cmds.duplicate(str(jnList[3]), n= str(jnList[3] + '_IK'), parentOnly=True)[0]
-    pm.parent(IKwrist, IKelbow)
-    
-    
-    
+    # create IK jnt chain
+    IKChain = cmds.duplicate(str(jnList[1]), n= str(jnList[1] + '_IK'))[0]
+    pm.parent(IKChain, world=True )
+     
+    # create FK jnt chain
     FKChain = cmds.duplicate(str(jnList[1]), n= str(jnList[1] + '_FK'))[0]
     pm.parent(FKChain, world=True )
     
-    dagObj = pm.listRelatives(FKChain, ad=True, type="joint")
-    for x in dagObj:
-        print x
-        name = x
+      
+    
+    # rename chains
+    dagObjFK = pm.listRelatives(FKChain, ad=True, type="joint")
+    dagObjIK = pm.listRelatives(IKChain, ad=True, type="joint")
+    
+    for x in dagObjFK:
         pm.rename(x, str(x) + '_FK')
+        FKJntList.append(x)
+        
+    for y in dagObjIK:
+        pm.rename(y, str(y) + '_IK') 
+        IKJntList.append(y)   
+        
+    IKJntList.append(IKChain)
+    FKJntList.append(FKChain)
+      
+    IKJntList = IKJntList[::-1]
+    FKJntList = FKJntList[::-1]
+    
+    
+    CreateIK(IKJntList)    
+    
     
 # ================================ # 
 
 def CreateArm(jntRadius):
-
+    
+    # create simple arm jnts
     clavicle = pm.joint(n = str(side) + 'clavicle_jnt', p = (1,0,0), rad = jntRadius) 
     shoulder = pm.joint(n = str(side) + 'shoulder_jnt', p = (2,0.3,0.4), rad = jntRadius) 
     elbow = pm.joint(n = str(side) + 'elbow_jnt', p = (3.7,0,0), rad = jntRadius) 
@@ -72,17 +120,19 @@ def CreateArm(jntRadius):
     dist = Distance(shoulder,elbow)
     wrist = pm.joint(n = str(side) + 'wrist_jnt', p = ((3.7 + dist),-0.3,0.4), rad = jntRadius) 
 
+    # set jnt orientation
     pm.joint(clavicle, e=True, zso = True, oj='xyz', sao = 'yup')
     pm.joint(shoulder, e=True, zso = True, oj='xyz', sao = 'yup')
     pm.joint(elbow, e=True, zso = True, oj='xyz', sao = 'yup')
     pm.joint(wrist, e=True, zso = True, oj='none')
 
+    # add jnts to list & create IK FK jnts
     jntList.append(clavicle)
     jntList.append(shoulder)
     jntList.append(elbow)
     jntList.append(wrist)
     
-    DuplicateChain(jntList)
+    IK_FKChain(jntList)
     
     
     # create twist joints    
@@ -94,10 +144,22 @@ def CreateArm(jntRadius):
     CreateTwistJnt(twistJntRadius, 'radius_twist_jnt', side, elbow, wrist, True, True)
     CreateTwistJnt(twistJntRadius, 'wrist_twist_jnt', side, wrist, elbow, False, False)
 
+ 
+
 
 CreateArm(0.5)
+
+"""
+print "____________________________________"    
+for x in IKJntList:
+    print x
+
+print "____" 
+for x in FKJntList:
+    print x
 
 print "____________________________________"    
 for x in jntList:
     print x
 
+"""

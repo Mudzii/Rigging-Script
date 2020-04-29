34a9fde79d32faeefa07250d2190cbf20ac41cc5
Eva S <37833950+Mudzii@users.noreply.github.com>
2020-04-29 09:37:02 +0200

    Update createJoints.py

--- createJoints.py
+++ createJoints.py
@@ -8,7 +8,6 @@ import maya.cmds as cmds
 
 #  ================= Function to create twist joints =================================== #
 #def CreateTwistJnt(jntList, jntRadius, jntName, prefix, prntJnt, nxtJnt, moveConst, Reparent):
-    
 def CreateTwistJnt(jntList, prefix, jntName, prntJnt, nxtJnt, jntRadius, moveConst, Reparent):
     
     twistJnt = cmds.duplicate(str(prntJnt), n= str(prefix) + str(jntName), parentOnly=True)[0]
@@ -33,3 +32,5 @@ def CreateTwistJnt(jntList, prefix, jntName, prntJnt, nxtJnt, jntRadius, moveCon
 
     # add jnts to list & create IK FK jnts    
     jntList.extend([clavicle, shoulder, elbow, wrist])
+
+#  ================= Function to create IK/FK Chain =================================== #
